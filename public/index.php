613057e6ab0e8957c67d683d0e5aad7d7a55818d
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-02-24 16:15:24 +0000

    Bump to version 0.1.0

--- public/index.php
+++ public/index.php
@@ -4,4 +4,9 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Core\Router;
 
 header('Content-type: application/json');
-http_response_code 
+http_response_code (200);
+define('RAPID_IN', TRUE);
+
+$inPath = trim(str_replace('index.php', '', trim($_SERVER['SCRIPT_NAME'], '\/\\' )), '\/\\');
+if( strlen(trim($inPath)) > 0){
+    define('INDEX_
