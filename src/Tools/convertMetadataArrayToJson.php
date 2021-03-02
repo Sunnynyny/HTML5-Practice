af5d538546788001464baca5aa7bc2b0d71e49d8
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-03-02 18:43:09 +0000

    Try to fix travis build

--- src/Tools/convertMetadataArrayToJson.php
+++ src/Tools/convertMetadataArrayToJson.php
@@ -5,4 +5,10 @@ if(PHP_SAPI != 'cli' || $argc < 1){
 }
 
 if(
-   
+    $argc < 2 ||
+    strlen($argv[1]) < 1 ||
+    !is_file($argv[1])
+){
+    exit(
+        "\n" .
+        'Usage: php convertMetadataArrayToJson.php <path_to_file> <output_f
