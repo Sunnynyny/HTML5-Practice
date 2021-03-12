ec3abbc8f377a6f8bcefc42a54556b2104a31e16
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-03-12 22:23:09 +0000

    Update external files

--- src/Tools/convertMetadataToCustom.php
+++ src/Tools/convertMetadataToCustom.php
@@ -5,3 +5,11 @@ if(PHP_SAPI != 'cli' || $argc < 1){
 }
 
 if(
+    $argc < 3 ||
+    strlen($argv[1]) < 1 ||
+    !is_file($argv[1]) ||
+    strlen($argv[2]) < 1 ||
+    !is_dir($argv[2])
+){
+    exit(
+        "\n" .
