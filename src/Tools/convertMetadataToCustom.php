e2507f1085d0e874535277623bff79a6fb2ff75b
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-03-14 11:09:29 +0000

    preparing release 4.0

--- src/Tools/convertMetadataToCustom.php
+++ src/Tools/convertMetadataToCustom.php
@@ -15,4 +15,6 @@ if(
         "\n" .
         'Usage: php convertMetadataToCustom.php <path_to_metadata.json> <output_path_to_file> <base API URL>' . "\n" .
         'Where:' . "\n" .
-       
+        "    <path_to_metadata.json> is path to metadata file\n" .
+        "    <output_path_to_file> is path to folder for output custom file\n" .
+        "    <base API URL> not requ
