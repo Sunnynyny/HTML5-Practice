01b8c72c5f1a2031dfa335c1747bfe24a6dec834
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-04-14 17:04:51 +0000

    Update support.lua

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -3,4 +3,4 @@
 return array (
     'package' => 'Coinbase',
     'tagline' => 'Make bitcoin/ethereum transactions and get real-time data.',
-    'description' =>
+    'description' => 'Connect to the Coinbase Digital Currency API to make bitcoin/ethereum transactions and get real-time data. Test an API call and export the code into your a
