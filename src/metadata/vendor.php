4c26c9083ba24023cc8d08d1a39e1b6cd5a27315
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-04-14 17:34:59 +0000

    Correct error message.

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -3,4 +3,6 @@
 return array (
     'package' => 'Coinbase',
     'tagline' => 'Make bitcoin/ethereum transactions and get real-time data.',
-    'description' => 'Connect to the Coinbase Digital Currency API to make bitcoin/ethereum transactions and get real-time data. Test an API call and export the code into your a
+    'description' => 'Connect to the Coinbase Digital Currency API to make bitcoin/ethereum transactions and get real-time data. Test an API call and export the code into your app.',
+    'image' => 'https://logo.clearbit.com/coinbase.com?s=128',
+    'repo' => 'https://github.com/RapidSoftwareSolutions/Marketplace-Co
