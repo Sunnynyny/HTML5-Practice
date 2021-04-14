9044803f20cd04572b3ac494af633d4d6875c028
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-04-14 18:01:51 +0000

    Bump version to 3.0

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -5,4 +5,9 @@ return array (
     'tagline' => 'Make bitcoin/ethereum transactions and get real-time data.',
     'description' => 'Connect to the Coinbase Digital Currency API to make bitcoin/ethereum transactions and get real-time data. Test an API call and export the code into your app.',
     'image' => 'https://logo.clearbit.com/coinbase.com?s=128',
-    'repo' => 'https://github.com/RapidSoftwareSolutions/Marketplace-Co
+    'repo' => 'https://github.com/RapidSoftwareSolutions/Marketplace-Coinbase-Package',
+    'keywords' => array (
+            'API', 'Bitcoin', 'Ethereum', 'Coinbase',
+        ),
+    'accounts' => array (
+            'domain' => 'coinbas
