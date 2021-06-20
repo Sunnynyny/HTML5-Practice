4a42c2bf9a6d42a3818c01150229a48c60327d09
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-06-20 06:29:35 +0000

    Yay!

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -220,4 +220,8 @@ return array (
                     'type' => 'String',
                     'info' => 'Access token.',
                     'required' => true,
-           
+                ),
+                array (
+                    'name' => 'accountId',
+                    'type' => 'String',
+                    'info' => 'Current user acco
