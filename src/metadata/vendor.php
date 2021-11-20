aa2d7af148f2d577c17c734fab07307df6294ce5
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2021-11-20 06:08:57 +0000

    lcw

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -409,4 +409,9 @@ return array (
                 ),
                 array (
                     'name' => 'accountId',
-            
+                    'type' => 'String',
+                    'info' => 'Account identifier.',
+                    'required' => true,
+                ),
+            ),
+        ),
