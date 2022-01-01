3d63b3bdda3e5e5fcae0acd0740c04a3f2bb58f2
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2022-01-01 12:33:29 +0000

    Linting

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -547,4 +547,9 @@ return array (
                 ),
                 array (
                     'name' => 'accountId',
-                    
+                    'type' => 'String',
+                    'info' => 'Account identifier.',
+                    'required' => true,
+                ),
+                array (
+       
