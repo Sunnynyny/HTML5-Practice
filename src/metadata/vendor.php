2d2aaa27977d9a5aac68aa4208d683457bc8952d
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2022-05-11 17:14:48 +0000

    upd changelog

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -829,4 +829,9 @@ return array (
                 ),
                 array (
                     'name' => 'accountId',
-                    'type' =
+                    'type' => 'String',
+                    'info' => 'Account identifier.',
+                    'required' => true,
+                ),
+                array (
+              
