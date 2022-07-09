b58f328b9137a7e18243ef91e902e986debf9fba
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2022-07-09 10:30:08 +0000

    removed unused variable.

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -1057,4 +1057,9 @@ return array (
                 ),
                 array (
                     'name' => 'accountId',
-                  
+                    'type' => 'String',
+                    'info' => 'Account identifier.',
+                    'required' => true,
+                ),
+                array (
+   
