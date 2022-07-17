86d66ac571394f8d24e93d3d2e3316ad7993f554
Sunnynyny <94838178+Sunnynyny@users.noreply.github.com>
2022-07-17 18:56:54 +0000

    mempolicy: remove mempolicy sharing

--- src/metadata/vendor.php
+++ src/metadata/vendor.php
@@ -1100,4 +1100,9 @@ return array (
                 array (
                     'name' => 'accountId',
                     'type' => 'String',
-                   
+                    'info' => 'Account identifier.',
+                    'required' => true,
+                ),
+                array (
+                    'name' => 'withdrawalId',
+      
